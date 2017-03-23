2b7e2e22c403d975d3f244653f53b2aba26e5e0c
Chewbaccamaster <shadowkrav@gmail.com>
2017-03-23 21:12:29 +0300

    Improtant Update!
    Переписал програму без использования списка смежностей, модифицировал
    бфс так, что сразу смотрит в 4 стороны куда идти

--- Lab2/Lab2/Source.cpp
+++ Lab2/Lab2/Source.cpp
@@ -7,20 +7,29 @@
 
 using namespace std;
 
+struct Vertex
+{
+	int x1;
+	int y1;
+	int x2;
+	int y2;
+};
 
-int N, R1, C1, R2, C2, start;
+int N, R1, C1, R2, C2;
 ifstream fin("input.txt");
 ofstream fout("output.txt");
 vector<vector<int>> maze1,maze2;
 vector<vector<int>> g;
-
+Vertex start;
 
 void create_mazes() {
 	fin >> N >> R1 >> C1 >> R2 >> C2;
 	maze1.resize(N);
 	maze2.resize(N);
-	start = R1*N*N*N + C1*N*N + R2*N + C2;
-	
+	start.x1 = R1;
+	start.y1 = C1;
+	start.x2 = R2;
+	start.y2 = C2;
 	
 	for (int i = 0;i <N;++i) {
 		maze1[i].reserve(N);
@@ -42,15 +51,6 @@ void create_mazes() {
 	fin.close();
 }
 
-void print_maze(vector<vector<int>> v) {
-	for (int i = 0;i < N; i++) {
-		for (int j = 0;j < N;j++) {
-			cout << v[i][j] << ' ';
-		}
-		cout << endl;
-	}
-}
-
 bool check_wall(vector<vector<int>>&v,int a1, int b1) {
 	if (a1 >= 0 && a1 < N&&b1>=0 && b1 < N)
 	{
@@ -62,90 +62,92 @@ bool check_wall(vector<vector<int>>&v,int a1, int b1) {
 	return true;
 }
 
-int go_up(int a, int b, int c, int d) {
-	int x = a, y = c;
-	if (check_wall(maze1, a - 1, b))
-		x -= 1;
-	if (check_wall(maze2, c - 1, d))
-		y -= 1;
-	return x*N*N*N + b*N*N + y*N + d;
+Vertex go_up(Vertex in) {
+	Vertex tempv;
+	tempv = in;
+	if (check_wall(maze1, tempv.x1 - 1, tempv.y1))
+		tempv.x1 -= 1;
+	if (check_wall(maze2, tempv.x2 - 1, tempv.y2))
+		tempv.x2 -= 1;
+	return tempv;
 }
 
-int go_down(int a, int b, int c, int d) {
-	int x = a, y = c;
-	if (check_wall(maze1, a + 1, b))
-		x += 1;
-	if (check_wall(maze2, c + 1, d))
-		y += 1;
-	return x*N*N*N + b*N*N + y*N + d;
+Vertex go_down(Vertex in) {
+	Vertex tempv;
+	tempv = in;
+	if (check_wall(maze1, tempv.x1 + 1, tempv.y1))
+		tempv.x1 += 1;
+	if (check_wall(maze2, tempv.x2 + 1, tempv.y2))
+		tempv.x2 += 1;
+	return tempv;
 }
 
-
-int go_left(int a, int b, int c, int d) {
-	int x = b, y = d;
-	if (check_wall(maze1, a, b - 1))
-		x -= 1;
-	if (check_wall(maze2, c, d - 1))
-		y -= 1;
-	return a*N*N*N + x*N*N + c*N + y;
+Vertex go_left(Vertex in) {
+	Vertex tempv;
+	tempv = in;
+	if (check_wall(maze1, tempv.x1, tempv.y1 - 1))
+		tempv.y1 -= 1;
+	if (check_wall(maze2, tempv.x2, tempv.y2 - 1))
+		tempv.y2 -= 1;
+	return tempv;
 }
 
-int go_right(int a, int b, int c, int d) {
-	int x = b, y = d;
-	if (check_wall(maze1, a, b + 1))
-		x += 1;
-	if (check_wall(maze2, c, d + 1))
-		y += 1;
-	return a*N*N*N + x*N*N + c*N + y;
+Vertex go_right(Vertex in) {
+	Vertex tempv;
+	tempv = in;
+	if (check_wall(maze1, tempv.x1, tempv.y1 + 1))
+		tempv.y1 += 1;
+	if (check_wall(maze2, tempv.x2, tempv.y2 + 1))
+		tempv.y2 += 1;
+	return tempv;
 }
 
-
-void create_list() {
-	for (int i1 = 0;i1 < N;i1++) 
-		for (int j1 = 0; j1 < N;j1++)
-			for (int i2 = 0;i2 < N;i2++)
-				for (int j2 = 0;j2 < N;j2++)
-				{
-					vector<int> v;
-					int up, left, down, right;
-					up = go_up(i1, j1, i2, j2);
-					left = go_left(i1, j1, i2, j2);
-					down = go_down(i1, j1, i2, j2);
-					right = go_right(i1, j1, i2, j2);
-
-					v.push_back(up);
-					v.push_back(down);
-					v.push_back(left);
-					v.push_back(right);
-
-					g.push_back(v);
-				}
-	
+int convert_coord(Vertex ver) {
+	return ver.x1*N*N*N + ver.y1*N*N + ver.x2*N + ver.y2;
 }
 
-
-int bfs(int s)
-{	
+int bfs(Vertex s)
+{
 	vector<bool> used(N*N*N*N);
 	vector<int> d(N*N*N*N);
-	queue<int> q;
+	queue<Vertex> q;
 	fill(used.begin(), used.end(), false);
 	fill(d.begin(), d.end(), 0);
 	q.push(s);
-	used[s] = true;
-	while (!q.empty()) {
-		int v = q.front();
-		q.pop();
-		for (size_t i = 0; i<g[v].size(); ++i) {
-			int to = g[v][i];
-			if (!used[to]) {
-				used[to] = true;
-				q.push(to);
-				d[to] = d[v] + 1;			
-			}
-		}
-	}
 
+	used[convert_coord(s)] = true;
+	while (!q.empty()) {
+		Vertex v = q.front();
+		q.pop();
+
+		Vertex up, down, left, right;
+		up = go_up(v);
+		down = go_down(v);
+		left = go_left(v);
+		right = go_right(v);
+
+		if (!used[convert_coord(up)]) {
+			used[convert_coord(up)] = true;
+			q.push(up);
+			d[convert_coord(up)] = d[convert_coord(v)] + 1;
+		}
+		if (!used[convert_coord(down)]) {
+			used[convert_coord(down)] = true;
+			q.push(down);
+			d[convert_coord(down)] = d[convert_coord(v)] + 1;
+		}
+		if (!used[convert_coord(left)]) {
+			used[convert_coord(left)] = true;
+			q.push(left);
+			d[convert_coord(left)] = d[convert_coord(v)] + 1;
+		}
+		if (!used[convert_coord(right)]) {
+			used[convert_coord(right)] = true;
+			q.push(right);
+			d[convert_coord(right)] = d[convert_coord(v)] + 1;
+		}
+		
+	}
 	return d[0];
 }
 
@@ -153,18 +155,9 @@ int bfs(int s)
 int main() {
 	int rez;
 	create_mazes();
-
-	create_list();	
-
 	rez = bfs(start);
-	
 	fout << rez;
 	fout.close();
-	
-
-
-
-
 	return EXIT_SUCCESS;
 }
 
