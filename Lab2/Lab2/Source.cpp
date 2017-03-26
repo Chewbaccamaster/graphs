216f860f99d0cb2a4b4ac65346263b849220f744
Chewbaccamaster <shadowkrav@gmail.com>
2017-03-26 15:21:57 +0300

    Update
    Переписал функцию движения и проверки на использованность вершины -
    теперь они универсальны для входных данных;

--- Lab2/Lab2/Source.cpp
+++ Lab2/Lab2/Source.cpp
@@ -20,16 +20,21 @@ ifstream fin("input.txt");
 ofstream fout("output.txt");
 vector<vector<int>> maze1,maze2;
 Vertex start;
+queue<Vertex> q;
+vector<bool> used;
+vector<int> d;
 
 void create_mazes() {
 	fin >> N >> R1 >> C1 >> R2 >> C2;
 	maze1.resize(N);
 	maze2.resize(N);
+	used.resize(N*N*N*N);
+	d.resize(N*N*N*N);
 	start.x1 = R1;
 	start.y1 = C1;
 	start.x2 = R2;
 	start.y2 = C2;
-	
+
 	for (int i = 0;i <N;++i) {
 		maze1[i].reserve(N);
 		for (int j = 0;j < N;++j) {
@@ -47,12 +52,12 @@ void create_mazes() {
 			maze2[i].push_back(peak);
 		}
 	}
+	
 	fin.close();
 }
 
-bool check_wall(vector<vector<int>>&v,int a1, int b1) {
-	if (a1 >= 0 && a1 < N&&b1 >= 0 && b1 < N)
-	{
+bool check_wall(vector<vector<int>>&v, int a1, int b1) {
+	if (a1 >= 0 && a1 < N && b1 >= 0 && b1 < N)	{
 		if (v[a1][b1] == 1) 
 			return false;
 		}
@@ -61,101 +66,67 @@ bool check_wall(vector<vector<int>>&v,int a1, int b1) {
 	return true;
 }
 
-Vertex go_up(Vertex &in) {
-	Vertex tempv;
-	tempv = in;
-	if (check_wall(maze1, tempv.x1 - 1, tempv.y1))
-		tempv.x1 -= 1;
-	if (check_wall(maze2, tempv.x2 - 1, tempv.y2))
-		tempv.x2 -= 1;
+Vertex move(Vertex &in, int a,  int b) {
+	Vertex tempv = in;
+	if (check_wall(maze1, tempv.x1 + a, tempv.y1 + b)) {
+		tempv.x1 += a;
+		tempv.y1 += b;
+	}
+		
+	if (check_wall(maze2, tempv.x2 + a, tempv.y2 + b)) {
+		tempv.x2 += a;
+		tempv.y2 += b;
+	}
 	return tempv;
-}
-
-Vertex go_down(Vertex &in) {
-	Vertex tempv;
-	tempv = in;
-	if (check_wall(maze1, tempv.x1 + 1, tempv.y1))
-		tempv.x1 += 1;
-	if (check_wall(maze2, tempv.x2 + 1, tempv.y2))
-		tempv.x2 += 1;
-	return tempv;
-}
-
-Vertex go_left(Vertex &in) {
-	Vertex tempv;
-	tempv = in;
-	if (check_wall(maze1, tempv.x1, tempv.y1 - 1))
-		tempv.y1 -= 1;
-	if (check_wall(maze2, tempv.x2, tempv.y2 - 1))
-		tempv.y2 -= 1;
-	return tempv;
-}
-
-Vertex go_right(Vertex &in) {
-	Vertex tempv;
-	tempv = in;
-	if (check_wall(maze1, tempv.x1, tempv.y1 + 1))
-		tempv.y1 += 1;
-	if (check_wall(maze2, tempv.x2, tempv.y2 + 1))
-		tempv.y2 += 1;
-	return tempv;
+		
 }
 
 int convert_coord(Vertex &ver) {
 	return ver.x1*N*N*N + ver.y1*N*N + ver.x2*N + ver.y2;
 }
 
-int bfs(Vertex s)
+void check_for_used(Vertex &move, Vertex &ver)
 {
-	vector<bool> used(N*N*N*N);
-	vector<int> d(N*N*N*N);
-	queue<Vertex> q;
-	fill(used.begin(), used.end(), false);
-	fill(d.begin(), d.end(), 0);
-	q.push(s);
+	if (!used[convert_coord(move)]) {
+		used[convert_coord(move)] = true;
+		q.push(move);
+		d[convert_coord(move)] = d[convert_coord(ver)] + 1;
+	}
+}
 
-	used[convert_coord(s)] = true;
+void bfs(Vertex s)
+{
+	q.push(s);
+ 	used[convert_coord(s)] = true;
 	while (!q.empty()) {
+		if (d[0] != 0)
+		{
+			return;
+		}
 		Vertex v = q.front();
 		q.pop();
 
 		Vertex up, down, left, right;
-		up = go_up(v);
-		down = go_down(v);
-		left = go_left(v);
-		right = go_right(v);
+		up = move(v, -1, 0);
+		down = move(v, 1, 0);
+		left = move(v, 0, -1);
+		right = move(v, 0, 1);
 
-		if (!used[convert_coord(up)]) {
-			used[convert_coord(up)] = true;
-			q.push(up);
-			d[convert_coord(up)] = d[convert_coord(v)] + 1;
-		}
-		if (!used[convert_coord(down)]) {
-			used[convert_coord(down)] = true;
-			q.push(down);
-			d[convert_coord(down)] = d[convert_coord(v)] + 1;
-		}
-		if (!used[convert_coord(left)]) {
-			used[convert_coord(left)] = true;
-			q.push(left);
-			d[convert_coord(left)] = d[convert_coord(v)] + 1;
-		}
-		if (!used[convert_coord(right)]) {
-			used[convert_coord(right)] = true;
-			q.push(right);
-			d[convert_coord(right)] = d[convert_coord(v)] + 1;
-		}
+		check_for_used(up,v);
+		check_for_used(down, v);
+		check_for_used(left, v);
+		check_for_used(right, v);
 		
 	}
-	return d[0];
+	
 }
 
-
 int main() {
-	int rez;
+	
 	create_mazes();
-	rez = bfs(start);
-	fout << rez;
+	bfs(start);
+	fout << d[0];
 	fout.close();
+
 	return EXIT_SUCCESS;
 }
