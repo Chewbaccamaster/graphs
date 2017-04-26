a2efad623a40d64794d0f514ad207132f9979ee8
Chewbaccamaster <shadowkrav@gmail.com>
2017-04-26 22:48:05 +0300

    upd
    well it still doesn't work, graph isn't built correcty probably

--- Lab4/Lab4/Source.cpp
+++ Lab4/Lab4/Source.cpp
@@ -9,7 +9,7 @@ using namespace std;
 ifstream fin("input.txt");
 ofstream fout("output.txt");
 int H, V, vertexnum = 0, Wprice, Kprice = 0;
-vector<int> p;
+vector<int> p, rang;
 vector <pair<int, pair<int, int>>> g;
 
 struct Word
@@ -34,7 +34,7 @@ void create_vector(vector<Word> &vec1, vector<Word> &vec2) {
 		temp.r = a; 
 		temp.c1 = b; 
 		temp.c2 = c; 
-		temp.cost = c - b+1;
+		temp.cost = c - b + 1;
 		temp.intersec.resize(0);
 
 		vec1.emplace_back(temp);
@@ -45,9 +45,9 @@ void create_vector(vector<Word> &vec1, vector<Word> &vec2) {
 		fin >> a >> b >> c; // r1 r2 c	 0 5 1
 
 							// r1 r2 c   0 5 3
-		temp.r = a; 
-		temp.c1 = b;
-		temp.c2 = c; 
+		temp.r = c; 
+		temp.c1 = a;
+		temp.c2 = b; 
 		temp.cost = b - a + 1;
 		temp.intersec.resize(0);
 
@@ -72,17 +72,17 @@ int count_cost(vector<Word> &vec1, vector<Word> &vec2)
 // hor:  1 0 5
 // ver:  0 5 1
 
-void find_intersections(vector<Word> &v, vector<Word> &h, const int N1, const int N2, int &cnt)
+void find_intersections(vector<Word> &h, vector<Word> &v, const int N1, const int N2, int &cnt)
 {
 	
 	for (int i = 0; i < N1;++i)
 		for (int j = 0;j < N2;++j)
 		{
-			if (v[i].r >= h[j].r)
-				if (v[i].c1 <= h[j].c2)
+			if (h[i].r >= v[j].c1 && h[i].r <= v[j].c2 && v[j].r)
+
 				{		
-					v[i].intersec.emplace_back(h[j].c2);
-					h[j].intersec.emplace_back(v[i].r);
+					h[i].intersec.emplace_back(v[j].r);
+					v[j].intersec.emplace_back(h[i].r);
 					cnt += 1;
 				}	
 		}
@@ -90,57 +90,57 @@ void find_intersections(vector<Word> &v, vector<Word> &h, const int N1, const in
 	
 }
 
-void create_graph(vector<Word> &v, vector<Word> &h, const int N1, const int N2,vector <pair<int, pair<int, int>>> &adjG)
+void create_graph(vector<Word> &h, vector<Word> &v, const int N1, const int N2,vector <pair<int, pair<int, int>>> &adjG)
 {
-	for (int j = 0;j < v[0].intersec.size() - 1;++j) {
-		pair<int, pair<int, int>> p;
-		p.second.first = j;
-		p.second.second = j + 1;
-		p.first = v[0].intersec[j + 1] - v[0].intersec[j] - 1;
-		adjG.emplace_back(p);
-	}
 
-	for (int i = 1;i < N1;++i)
-		for (int j = 0;j < v[i].intersec.size() - 1;++j) {
-			pair<int, pair<int, int>> p;
-			p.second.first = i + v[i - 1].intersec.size() - 1;
-			p.second.second = i + v[i - 1].intersec.size();
-			p.first = v[i].intersec[j + 1] - v[i].intersec[j] - 1;
-			adjG.emplace_back(p);
-		}
-		
-
-	for (int j = 0;j < h[0].intersec.size() - 1;++j) {
-		pair<int, pair<int, int>> p;
-		p.second.first = j;
-		p.second.second = v[j].intersec.size();
-		p.first = h[0].intersec[j + 1] - h[0].intersec[j] - 1;
-		adjG.emplace_back(p);
-	}
-
-	for (int i = 1;i < N2;++i)
+	int a = 0, b = 1;
+	for (int i = 0;i < N1;++i){
+		if (h[i].intersec.size() > 0)
 		for (int j = 0;j < h[i].intersec.size() - 1;++j) {
 			pair<int, pair<int, int>> p;
-			p.second.first = v[i - 1].intersec.size() - 1;
-			p.second.second = i + v[i - 1].intersec.size();
-			p.first = h[i].intersec[j + 1] - h[i].intersec[j] - 1;
+			
+			p.second.first = a+j;
+			p.second.second = b;
+			p.first = abs(h[i].intersec[j + 1] - h[i].intersec[j] - 1);
+			adjG.emplace_back(p);
+			a += 2;
+			b += 2;
+		}
+	}
+
+
+	for (int i = 0;i < N2;++i) {
+		if (v[i].intersec.size() > 0)
+		for (int j = 0;j < v[i].intersec.size() - 1;++j) {
+			pair<int, pair<int, int>> p;
+			p.second.first = i+j;	
+			p.second.second = i + h[i].intersec.size();
+			p.first = abs(v[i].intersec[j + 1] - v[i].intersec[j]-1);
 			adjG.emplace_back(p);
 		}
-			
-		
+	}
+	
 }
 
+
+
 int dsu_get(int v) {
-	return (v == p[v]) ? v : (p[v] = dsu_get(p[v]));
+	if (v == p[v])
+		return v;
+	return p[v] = dsu_get(p[v]);
 }
 
 void dsu_unite(int a, int b) {
-	a = dsu_get(a);
-	b = dsu_get(b);
-	if (rand() & 1)
-		swap(a, b);
-	if (a != b)
-		p[a] = b;
+	int	ra = dsu_get(a);
+	int rb = dsu_get(b);
+	if (ra == rb)
+		return;
+	if (rang[ra] > rang[rb])
+		swap(ra, rb);
+	p[ra] = rb;
+	if (rang[ra] == rang[rb])
+		rang[rb]++;
+
 }
 
 void kruskal(vector <pair<int, pair<int, int>>> &adjG, const int cnt, int &c)
@@ -148,25 +148,73 @@ void kruskal(vector <pair<int, pair<int, int>>> &adjG, const int cnt, int &c)
 	int cost = 0, n = cnt, m = adjG.size();
 	sort(adjG.begin(), adjG.end());
 	p.resize(n);
+	rang.resize(n,0);
+	for (int i = 0; i < n; ++i) 
+		p[i] = i;
+		
+	for (int i = 0; i < m; ++i) {
+		int a = g[i].second.first, b = g[i].second.second, l = g[i].first;
+		if (dsu_get(a) == dsu_get(b))
+			if (l == 0) {
+				fout << -1 << endl;
+				fout.close();
+				return;
+			}
+			else {
+				cost += l;
+				continue;
+		}
+			dsu_unite(a, b);
+			
+		
+	}
+	c = cost;	
+}
 
+
+/*void kruskal_rb(vector <pair<int, pair<int, int>>> &adjG, const int cnt, int &c)
+{
+	int cost = 0, n = cnt, m = adjG.size();
+	sort(adjG.begin(), adjG.end());
+	p.resize(n);
+	rang.resize(n, 0);
 	for (int i = 0; i < n; ++i)
 		p[i] = i;
 
-	for (int i = 0; i<m; ++i) {
+	for (int i = 0; i < m; ++i) {
 		int a = g[i].second.first, b = g[i].second.second, l = g[i].first;
-		if (dsu_get(a) != dsu_get(b)) {
+		if (dsu_get(a) == dsu_get(b))
+			continue;
+		dsu_unite(a, b);
+		cost++;
+		if (cost == n - 1)
+			break;
+	}
+	c = cost;
+}*/
+
+/*void kruskal1(vector < pair < int, pair<int, int> > > &adjG, const int cnt, int &c) {
+	int cost = 0, n = cnt, m = adjG.size();
+
+	sort(adjG.begin(), adjG.end());
+	vector<int> tree_id(n);
+	for (int i = 0; i<n; ++i)
+		tree_id[i] = i;
+	for (int i = 0; i<m; ++i)
+	{
+		int a = g[i].second.first, b = g[i].second.second, l = g[i].first;
+		if (tree_id[a] != tree_id[b])
+		{
 			cost += l;
-			dsu_unite(a, b);
-		}
-		else if (l == 0) {
-			fout << -1 << endl;
-			fout.close();
-			return;
+			int old_id = tree_id[b], new_id = tree_id[a];
+			for (int j = 0; j<n; ++j)
+				if (tree_id[j] == old_id)
+					tree_id[j] = new_id;
 		}
 	}
 	c = cost;
 }
-
+*/
 void print_result(int &Wp,int &Kp, int &vernum) {
 	fout << Wp - Kp - vernum << endl;
 	fout.close();
@@ -178,7 +226,7 @@ int main() {
 	find_intersections(hor, ver, H, V,vertexnum);
 
 	create_graph(hor, ver, H, V, g);
-
+//	kruskal1(g, vertexnum, Kprice);
 	kruskal(g, vertexnum, Kprice);
 
 	Wprice = count_cost(hor, ver);
@@ -186,13 +234,6 @@ int main() {
 	print_result(Wprice, Kprice, vertexnum);
 
 
-	/*
-	for (int i = 0; i < hor.size();++i)
-		cout << hor[i].r << ' ' << hor[i].c1 << ' ' << hor[i].c2  << endl;
-
-	for (int i = 0; i < hor.size();++i)
-		cout << ver[i].c1 << ' ' << hor[i].c2 << ' ' << hor[i].r << endl;
-		*/
 
 	return EXIT_SUCCESS;
 }
